43332b4a7e94ca97f8f86fcc61194d966fddbe70
talhaakpinar <64715935+talhaakpinar@users.noreply.github.com>
2021-01-12 16:09:50 +0300

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -2,24 +2,26 @@ vize1 = int(input("vize 1. sınav notu giriniz: "))
 vize2 = int(input("vize 2. sınav notu giriniz: "))
 final = int(input("final sınav notu giriniz: "))
 if 0 < vize1 < 100 and 0 < vize2 < 100 and 0 < final < 100:
-    toplam_not = (vize1 * 0.3) + (vize2 * 0.3) + (final * 0.140)
+    toplam_not = (vize1 * 0.3) + (vize2 * 0.3) + (final * 0.4)
     if toplam_not >= 90:
         print("harf notunuz: AA")
-    if toplam_not >= 85 and toplam_not < 90:
+    elif toplam_not >= 85 and toplam_not < 90:
         print("harf notunuz: BA")
-    if toplam_not >= 80 and toplam_not < 85:
+    elif toplam_not >= 80 and toplam_not < 85:
         print("harf notunuz: BB")
-    if toplam_not >= 75 and toplam_not < 80:
+    elif toplam_not >= 75 and toplam_not < 80:
         print("harf notunuz: CB")
-    if toplam_not >= 70 and toplam_not < 75:
+    elif toplam_not >= 70 and toplam_not < 75:
         print("harf notunuz: CC")
-    if toplam_not >= 65 and toplam_not < 70:
+    elif toplam_not >= 65 and toplam_not < 70:
         print("harf notunuz: DC")
-    if toplam_not >= 60 and toplam_not < 65:
+    elif toplam_not >= 60 and toplam_not < 65:
         print("harf notunuz: DD")
-    if toplam_not >= 55 and toplam_not < 60:
+    elif toplam_not >= 55 and toplam_not < 60:
         print("harf notunuz: FD")
-    if toplam_not < 55:
+    elif toplam_not < 55:
         print("harf notunuz: FF")
+elif (0 > vize1 or 100 < vize1) or (0 > vize2 or 100 < vize2) or (0 > final or 100 < final):
+    print("Lütfen 0 ile 100 arasında sayılar giriniz.")
 else:
     print("Harf notunuz FF Malesef kaldınız ")
